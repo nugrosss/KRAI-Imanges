4a9d60e80a3c4ee0c3356bd7d9788818260b0b6b
nugrosss <98314081+nugrosss@users.noreply.github.com>
2026-05-31 19:25:39 +0700

    update jarak

--- converopenvino.py
+++ converopenvino.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO(r"KRAI\rak-v2\weights\best.pt")
+model = YOLO(r"D:\kuliah JTD\belajar aroc\KRAI\rak-v2\weights\best.pt")
 
 # Export the model
 model.export(format="openvino",half=True,int8=True)  # creates 'yolo26n_openvino_model/'
--- main.py
+++ main.py
@@ -4,13 +4,15 @@ from grib import draw_grid_3x3,get_grid_cell,count_objects_in_grid
 import time
 
 
-model = YOLO(r"D:\kuliah JTD\belajar aroc\KRAI\rak-v2\weights\best_openvino_model", task="detect")
+model = YOLO(r"rak-v2\weights\best_int8_openvino_model", task="detect")
 
 
 cap = cv2.VideoCapture(r"D:\kuliah JTD\belajar aroc\KRAI\frame_2.mp4")
-
+# cap = cv2.VideoCapture(1)
  
 prev_time = 0
+tinggi_box = 35
+focal_length = 800
 
 
 
@@ -56,6 +58,8 @@ while True:
 
                 cx = (x1 + x2) // 2
                 cy = (y1 + y2) // 2
+                distance = (focal_length * tinggi_box) / (y2 - y1)
+                print(f"Jarak: {distance:.2f} cm")
 
                 kotak_list.append((cx, cy))
     
